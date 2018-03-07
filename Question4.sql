f1d2521cc63f0d0e44aed637203ff15318d863bf
Aburasumashi81104 <boostedzombie@gmail.com>
2018-03-07 13:21:16 +0900

    MySQL_Q4

--- Question4.sql
+++ Question4.sql
@@ -5,5 +5,4 @@ INSERT INTO item (item_name,item_price,category_id) VALUES
     ('おしゃれな椅子',2000,1),('こんがり肉',500,2),('書き方ドリルSQL',2500,3),
     ('ふわふわのベッド',30000,1),('ミラノ風ドリア',300,2);
  
-    
 SELECT * FROM item;
